10aa822dacbf542c0ae14b8f5b16241831b4f056
Michael Williamson <mike@zwobble.org>
2014-06-12 17:46:46 +0100

    Use geometry diagram when rendering angle examples

--- js/geometry-diagrams.js
+++ js/geometry-diagrams.js
@@ -2,9 +2,11 @@ var d3 = require("d3");
 
 exports.create = createGeometryDiagram;
 
+var defaultLineStrokeColor = "#3366ff";
+
 function createGeometryDiagram(options) {
     var parentElement = options.parentElement;
-    var padding = options.padding;
+    var padding = options.padding || 0;
     var width = options.width;
     var height = options.height;
     
@@ -19,23 +21,53 @@ function createGeometryDiagram(options) {
 function GeometryDiagram(svg, padding) {
     this._svg = svg;
     this._padding = padding;
-    this._paddingTransform = "translate(" + padding + "," + padding + ")";
 }
 
-GeometryDiagram.prototype.drawPath = function(path, options) {
+GeometryDiagram.prototype.drawPath = function(path) {
     this._svg.append("path")
         .datum(path)
         .attr("d", d3.svg.line())
-        .attr("transform", this._paddingTransform)
-        .style("stroke", options.strokeColor)
+        .attr("transform", this._translation(0, 0))
+        .style("stroke", defaultLineStrokeColor)
         .style("fill", "none");
 };
+
+GeometryDiagram.prototype.drawVertex = function(centre) {
+    this._svg.append("circle")
+        .attr("cx", centre.x)
+        .attr("cy", centre.y)
+        .attr("r", 5)
+        .style("fill", defaultLineStrokeColor);
+};
+
+GeometryDiagram.prototype.drawArm = function(options) {
+    var start = options.start;
+    var azimuth = options.azimuth;
+    var length = options.length;
+    
+    var end = calculateArmEnd(start, azimuth, length);
+    this._svg.append("line")
+        .attr("x1", start.x)
+        .attr("y1", start.y)
+        .attr("x2", end.x)
+        .attr("y2", end.y)
+        .style("stroke", defaultLineStrokeColor);
+};
+
+function calculateArmEnd(start, azimuth, length) {
+    return {
+        x: start.x + Math.sin(azimuth) * length,
+        y: start.y - Math.cos(azimuth) * length
+    };
+}
+
 
 GeometryDiagram.prototype.drawAngleMarker = function(vertex, azimuths) {
     var startAzimuth = Math.min(azimuths.start, azimuths.end);
     var endAzimuth = Math.max(azimuths.start, azimuths.end);
     
     var innerRadius = 20;
+    var strokeWidth = 1;
     
     if (isRightAngle(startAzimuth, endAzimuth)) {
         var squareWidth = innerRadius / 4 * 3;
@@ -45,7 +77,7 @@ GeometryDiagram.prototype.drawAngleMarker = function(vertex, azimuths) {
             .attr("y1", vertex.y - Math.cos(startAzimuth) * squareWidth)
             .attr("x2", vertex.x + Math.sin(startAzimuth) * squareWidth + Math.cos(startAzimuth) * squareWidth)
             .attr("y2", vertex.y - Math.cos(startAzimuth) * squareWidth + Math.sin(startAzimuth) * squareWidth)
-            .attr("transform", this._paddingTransform)
+            .attr("transform", this._translation(0, 0))
             .style("stroke", "#000");
         
         this._svg.append("line")
@@ -53,11 +85,25 @@ GeometryDiagram.prototype.drawAngleMarker = function(vertex, azimuths) {
             .attr("y1", vertex.y + Math.sin(startAzimuth) * squareWidth)
             .attr("x2", vertex.x + Math.cos(startAzimuth) * squareWidth + Math.sin(startAzimuth) * squareWidth)
             .attr("y2", vertex.y + Math.sin(startAzimuth) * squareWidth - Math.cos(startAzimuth) * squareWidth)
-            .attr("transform", this._paddingTransform)
+            .attr("transform", this._translation(0, 0))
             .style("stroke", "#000");
+    } else {
+        var arc = d3.svg.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(innerRadius + strokeWidth)
+            .startAngle(startAzimuth)
+            .endAngle(endAzimuth);
+            
+        this._svg.append("path")
+            .attr("d", arc)
+            .attr("transform", this._translation(vertex.x, vertex.y));
     }
 };
 
+    
+GeometryDiagram.prototype._translation = function(x, y) {
+    return "translate(" + (this._padding + x) + "," + (this._padding + y) + ")";
+};
 
 function isRightAngle(startAngle, endAngle) {
     return Math.abs(endAngle - startAngle - Math.PI / 2) < 0.000001;
--- js/types-of-angles.js
+++ js/types-of-angles.js
@@ -1,5 +1,4 @@
 var $ = require("jquery");
-var d3 = require("d3");
 
 var geometryDiagrams = require("./geometry-diagrams");
 
@@ -35,9 +34,7 @@ function renderRectangleAngleExampleWidget(element) {
         [0, 0]
     ];
     
-    diagram.drawPath(rectanglePath, {
-        strokeColor: "#3366ff"
-    });
+    diagram.drawPath(rectanglePath);
     
     diagram.drawAngleMarker({x: 0, y: 0}, {start: Math.PI / 2, end: Math.PI});
     diagram.drawAngleMarker({x: width, y: 0}, {start: Math.PI, end: Math.PI * 3 / 2});
@@ -48,9 +45,6 @@ function renderRectangleAngleExampleWidget(element) {
 renderAngleWidgets();
 
 function renderAngleWidgets() {
-    $("*[data-widget='display-angle']").each(function() {
-        renderAngleWidget(this);
-    });
     $("*[data-widget='angle-example']").each(function() {
         renderAngleExamplesWidget(this);
     });
@@ -61,15 +55,17 @@ function renderAngleExamplesWidget(element) {
     var angle = readAngleAttribute(element, "data-angle");
     var finalHeading = initialHeading + angle;
     
-    var svg = d3.select(element)
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
+    var diagram = geometryDiagrams.create({
+        parentElement: element,
+        width: width,
+        height: height
+    });
     
-    drawVertex(svg);
-    drawArm(svg, initialHeading);
-    drawArm(svg, finalHeading);
-    drawMarker(svg, centre, initialHeading, finalHeading);
+    diagram.drawVertex(centre);
+    diagram.drawAngleMarker(centre, {start: initialHeading, end: finalHeading});
+    var armLength = width / 2 * 0.8;
+    diagram.drawArm({start: centre, azimuth: initialHeading, length: armLength});
+    diagram.drawArm({start: centre, azimuth: finalHeading, length: armLength});
 }
 
 function readInitialHeading(element) {
@@ -92,87 +88,4 @@ function readAngleAttribute(element, name) {
 
 function randomAngle() {
     return Math.random() * Math.PI * 2;
-}
-
-function renderAngleWidget(element) {
-    var angleDegrees = element.getAttribute("data-angle");
-    var angle = angleDegrees / 180 * Math.PI;
-    
-    var svg = d3.select(element)
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height);
-    
-    drawVertex(svg);
-    drawArm(svg, 0);
-    drawArm(svg, angle);
-    drawMarker(svg, centre, 0, angle);
-}
-
-function drawArm(svg, angle) {
-    var to = calculateArmEnd(angle);
-    svg.append("line")
-        .attr("x1", centre.x)
-        .attr("y1", centre.y)
-        .attr("x2", to.x)
-        .attr("y2", to.y)
-        .style("stroke", "#3366ff");
-}
-
-function calculateArmEnd(angle) {
-    var armLength = width / 2 * 0.8;
-    return {
-        x: centre.x + Math.cos(angle) * armLength,
-        y: centre.y - Math.sin(angle) * armLength
-    };
-}
-
-function drawVertex(svg) {
-    var vertexFillColour = "#003399";
-    
-    svg.append("circle")
-        .attr("cx", centre.x)
-        .attr("cy", centre.y)
-        .attr("r", 5)
-        .style("fill", vertexFillColour);
-}
-
-function drawMarker(svg, position, startAngle, endAngle) {
-    var innerRadius = 20;
-    var strokeWidth = 1;
-    
-    if (isRightAngle(startAngle, endAngle)) {
-        var squareWidth = innerRadius / 4 * 3;
-        svg.append("line")
-            .attr("x1", position.x + Math.cos(endAngle) * squareWidth)
-            .attr("y1", position.y - Math.sin(endAngle) * squareWidth)
-            .attr("x2", position.x + Math.cos(endAngle) * squareWidth + Math.cos(startAngle) * squareWidth)
-            .attr("y2", position.y - Math.sin(endAngle) * squareWidth - Math.sin(startAngle) * squareWidth)
-            .style("stroke", "#000");
-            
-        svg.append("line")
-            .attr("x1", position.x + Math.cos(startAngle) * squareWidth)
-            .attr("y1", position.y - Math.sin(startAngle) * squareWidth)
-            .attr("x2", position.x + Math.cos(endAngle) * squareWidth + Math.cos(startAngle) * squareWidth)
-            .attr("y2", position.y - Math.sin(endAngle) * squareWidth - Math.sin(startAngle) * squareWidth)
-            .style("stroke", "#000");
-    } else {
-        var arc = d3.svg.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(innerRadius + strokeWidth)
-            .startAngle(polarAngleToD3Angle(startAngle))
-            .endAngle(polarAngleToD3Angle(endAngle));
-            
-    svg.append("path")
-        .attr("d", arc)
-        .attr("transform", "translate(" + position.x + "," + position.y + ")");
-    }
-}
-
-function polarAngleToD3Angle(angle) {
-    return -angle + Math.PI / 2;
-}
-
-function isRightAngle(startAngle, endAngle) {
-    return Math.abs(Math.abs(endAngle - startAngle) - Math.PI / 2) < 0.000001;
 }
