dd5115c7eb2acdeb9d5846de82b6464831d89c79
Michael Williamson <mike@zwobble.org>
2014-06-17 16:05:34 +0100

    Add a simple multiple choice test

--- js/types-of-angles.js
+++ js/types-of-angles.js
@@ -2,12 +2,63 @@ var $ = require("jquery");
 
 var geometryDiagrams = require("./geometry-diagrams");
 
-var width = 200;
-var height = width;
-
-var centre = {x: width / 2, y: height / 2};
-
 renderRectangleAngleWidgets();
+renderAngleWidgets();
+renderTestWidgets();
+
+function renderTestWidgets() {
+    $("*[data-widget='types-of-angles-test']").each(function() {
+        renderTestWidget(this);
+    });
+}
+
+function renderTestWidget(element) {
+    var question = generateAngleMultipleChoiceQuestion();
+    renderMultipleChoice(element, question);
+}
+
+function generateAngleMultipleChoiceQuestion() {
+    return {
+        text: "Which angle is larger?",
+        choices: [
+            {text: "Acute angle", isCorrect: false},
+            {text: "Obtuse angle", isCorrect: true}
+        ]
+    };
+}
+
+function renderMultipleChoice(element, question) {
+    $(element).addClass("test");
+    
+    var textElement = $("<p>")
+        .text(question.text)
+        .addClass("question-text");
+    $(element).append(textElement);
+    
+    var choicesElement = $("<ul>").addClass("choices");
+    $(element).append(choicesElement);
+    
+    question.choices.forEach(function(choice) {
+        var choiceElement = $("<li>")
+            .text(choice.text);
+        choicesElement.append(choiceElement);
+        
+        choiceElement.on("click", function() {
+            if (choice.isCorrect) {
+                resultElement.addClass("result-correct");
+                resultElement.removeClass("result-incorrect");
+                resultElement.text("Correct!");
+            } else {
+                resultElement.addClass("result-incorrect");
+                resultElement.removeClass("result-correct");
+                resultElement.text("Incorrect");
+            }
+        });
+    });
+    
+    var resultElement = $("<p>").addClass("result");
+    $(element).append(resultElement);
+}
 
 function renderRectangleAngleWidgets() {
     $("*[data-widget='rectangle-angle-example']").each(function() {
@@ -42,8 +93,6 @@ function renderRectangleAngleExampleWidget(element) {
     diagram.drawAngleMarker({x: width, y: height}, {start: 0, end: -Math.PI / 2});
 }
 
-renderAngleWidgets();
-
 function renderAngleWidgets() {
     $("*[data-widget='angle-example']").each(function() {
         renderAngleExamplesWidget(this);
@@ -51,6 +100,11 @@ function renderAngleWidgets() {
 }
 
 function renderAngleExamplesWidget(element) {
+    var width = 200;
+    var height = width;
+
+    var centre = {x: width / 2, y: height / 2};
+
     var initialHeading = readInitialHeading(element);
     var angle = readAngleAttribute(element, "data-angle");
     var finalHeading = initialHeading + angle;
